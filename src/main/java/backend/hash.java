c3600814c11457269cfabe7661f51949101db95b
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-12-15 16:39:22 +0530

    Added a Sha256 Hashing Function

--- src/main/java/backend/hash.java
+++ src/main/java/backend/hash.java
@@ -1,7 +1,15 @@
 package backend;
 
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+import java.nio.charset.StandardCharsets;
+
 public class hash {
 
+    private final HashFunction sha256_hasher = Hashing.sha256();
 
+    public String sha256(String a) {
+        return sha256_hasher.hashString(a, StandardCharsets.UTF_8).toString();
+    }
 
 }
