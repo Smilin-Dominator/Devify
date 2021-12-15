6c78b0a593b43d79612ab170364aa1bcd48bc483
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-12-15 16:38:47 +0530

    Foundation for UI

--- src/main/java/frontend/ui.java
+++ src/main/java/frontend/ui.java
@@ -1,7 +1,12 @@
 package frontend;
 
-public class ui {
+import javax.swing.JFrame;
 
+public class ui extends JFrame {
 
+    public ui() {
+        setSize(1080,1920);
+        setVisible(true);
+    }
 
 }
