ff6efc84de08711c871038b067ebab2a44ba5438
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-12-29 23:30:22 +0530

    Added 'verify_string' and 'compare_hashes' functions

--- src/main/java/backend/verify.java
+++ src/main/java/backend/verify.java
@@ -7,9 +7,18 @@ public class verify {
 
     private final hash hashing = new hash();
 
+    public boolean compare_hashes(String h1, String h2) {
+        return Objects.equals(h1, h2);
+    }
+
     public boolean verify_file(String path, String hash) throws IOException {
         String new_hash = hashing.file(path);
-        return Objects.equals(hash, new_hash);
+        return compare_hashes(hash, new_hash);
+    }
+
+    public boolean verify_string(String string, String hash) {
+        String new_hash = hashing.string(string);
+        return compare_hashes(hash, new_hash);
     }
 
 }
