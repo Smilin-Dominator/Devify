6fdb6870e38386ad7928243df74aaa8e7e6433c7
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-12-29 23:27:09 +0530

    Fixed the hash testing function

--- src/test/java/hashing.java
+++ src/test/java/hashing.java
@@ -37,7 +37,7 @@ public class hashing {
     @Test
     void hashFile() throws IOException {
 
-        String tbh = "This is another test\nlol!";
+        String real_hash = "abdc941476c8244ab0da7df485644f13ac0eb9cb662813a7a31a13b1b034312a";
 
         File ftbh = new File("tbh.txt");
         assert ftbh.createNewFile();
@@ -45,22 +45,22 @@ public class hashing {
         List<String> spl = Arrays.asList("This is another test", "lol!");
         Files.write(ftbh.toPath(), spl, StandardCharsets.UTF_8);
 
-        System.out.println("File Hash:  " + hashing_test.file(ftbh.getAbsolutePath()));
+        String file_hash = hashing_test.file(ftbh.getAbsolutePath());
+        boolean eq = Objects.equals(real_hash, file_hash);
 
-        /*
         System.out.println("------------------------ Hashing Test (File) ---------------------------");
-        System.out.println("The File                : " + tbh);
+        System.out.println("The File's Contents     : " + spl);
         System.out.println("The Real Hash           : " + real_hash);
-        System.out.println("The Program's Hash      : " + hashed);
+        System.out.println("The Program's Hash      : " + file_hash);
         System.out.println("Matched?                : " + eq);
-        */
+
+        assertTrue(eq);
 
     }
 
     @AfterAll
     static void clean() {
-        File hashed = new File("./tbh.txt");
-        assertTrue(hashed.delete());
+        new File("tbh.txt").delete();
     }
 
 }
