a98791af80764640b3b525cba752fa925d427aeb
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-12-29 22:19:31 +0530

    Made a file function for hashing

--- src/main/java/backend/hash.java
+++ src/main/java/backend/hash.java
@@ -3,7 +3,10 @@ package backend;
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class hash {
 
@@ -13,8 +16,29 @@ public class hash {
         return sha256_hasher.hashString(a, StandardCharsets.UTF_8).toString();
     }
 
-    public String file(String path) {
-        return "F";
+    public String file(String path) throws FileNotFoundException {
+
+        // Creating A File With The Path
+        File fil = new File(path);
+
+        // Exit if the Path does not exist
+        if (!fil.exists()) {
+            System.out.println("Path Does Not Exist!");
+            System.exit(1);
+        }
+
+        // Preparing to read the file
+        Scanner reader = new Scanner(fil);
+        StringBuilder build = new StringBuilder();
+
+        // Reading the file line by line
+        while (reader.hasNextLine()) {
+            build.append(reader.nextLine());
+        }
+
+        // Returning the hashed result
+        return string(build.toString());
+
     }
 
 }
