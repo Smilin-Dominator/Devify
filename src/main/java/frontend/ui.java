e8d60476b5f10ee400693dc9fc1a361864aefede
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-12-30 14:19:56 +0530

    Updated ui.java - Added a JFileChooser along with action listener functions for it - Set the window visible after adding all elements

--- src/main/java/frontend/ui.java
+++ src/main/java/frontend/ui.java
@@ -1,33 +1,47 @@
 package frontend;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class ui extends JFrame implements ActionListener {
+
+    private JFileChooser fileChooser;
 
     public ui() {
 
         // Bare Bones
         setSize(450,800);
         setTitle("Devify - The Verifier!");
-        setVisible(true);
 
         // Main Panel
         JPanel master = new JPanel();
         master.setLayout(new BorderLayout(0, 0));
 
+        // File Chooser
+        fileChooser = new JFileChooser();
+        fileChooser.addActionListener(this);
+
         // Addition
+        master.add(fileChooser);
         add(master);
+
+        // Final
+        setVisible(true);
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println(e.getActionCommand());
+        String ac_com = e.getActionCommand();
+        if (Objects.equals(ac_com, "ApproveSelection")) {
+            System.out.println(fileChooser.getSelectedFile());
+        } else if (Objects.equals(ac_com, "CancelSelection")) {
+            System.exit(0);
+        }
     }
 
 }
