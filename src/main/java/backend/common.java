c822f4eca8379b41ad73da518a39daa1d63b17e4
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-12-30 19:31:05 +0530

    JavaDoc for Common

--- src/main/java/backend/common.java
+++ src/main/java/backend/common.java
@@ -4,6 +4,12 @@ import java.util.Objects;
 
 public class common {
 
+    /**
+     * Essentially, checks if the filename is in the last element of the path split by '/'.
+     * @param filename The Filename, or the final string
+     * @param path The Path, or the main string
+     * @return True if it's the last string, False if it doesn't exist
+     */
     public boolean filename_in_path(String filename, String path) {
         String[] path_split = path.split("/");
         String[] file_split = filename.split("/");
