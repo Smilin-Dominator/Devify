0038403f49d1a8e84d1b3fc47e1db4c0b7d04d4f
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-12-31 10:04:20 +0530

    Added a test to verify strings

--- src/test/java/com/smilin_dominator/devify/test/verifying.java
+++ src/test/java/com/smilin_dominator/devify/test/verifying.java
@@ -1,6 +1,7 @@
 package com.smilin_dominator.devify.test;
 
 import com.smilin_dominator.devify.backend.verify;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -21,6 +22,18 @@ public class verifying {
 
         assertTrue(True);
         assertFalse(False);
+
+    }
+
+    @Test
+    public void string_hash() {
+
+        String string = "You Are A Donk!";
+        String hash = "f6fa9bb646aed5a65fe22c770935bce17b501ad8f6d63bd6f30c001878f0f3e5";
+
+        boolean True = Verify.verify_string(string, hash);
+
+        assertTrue(True);
 
     }
 
