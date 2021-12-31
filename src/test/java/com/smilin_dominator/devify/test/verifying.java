4575e28c86f911268ea6f19a30ed3eb4ad5cc2a9
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2021-12-31 11:46:10 +0530

    Made the tests run in a certain order Which is defined in FeatureTestSuite

--- src/test/java/com/smilin_dominator/devify/test/verifying.java
+++ src/test/java/com/smilin_dominator/devify/test/verifying.java
@@ -1,16 +1,19 @@
 package com.smilin_dominator.devify.test;
 
+import com.smilin_dominator.devify.backend.hash;
 import com.smilin_dominator.devify.backend.verify;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
