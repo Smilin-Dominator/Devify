0689ec224059e447785491e6dc64be82b121c664
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2022-01-01 13:17:20 +0530

    Preparing for merge

--- src/test/java/com/smilin_dominator/devify/test/verifying.java
+++ src/test/java/com/smilin_dominator/devify/test/verifying.java
@@ -26,7 +26,7 @@ public class verifying {
 
     @Test
     void two_hashes() {
-        System.out.println(1);
+
         String hash1 = "bf3f1a53a871ca130b3a238b3de8c757c6d9ba9614e8fc73ddb23daf003a8cd9";
         String hash2 = "bf3f1a53a871ca130b3a238b3de8c757c6d9ba9614e8fc73ddb23daf003a8cd9";
         String hash3 = "bf3f1a53a871ca130b3a238b3de8c757614e8fc73ddb23daf003a8cd9";
@@ -41,7 +41,6 @@ public class verifying {
 
     @Test
     void string_hash() {
-        System.out.println(2);
 
         String string = "You Are A Donk!";
         String hash = "f6fa9bb646aed5a65fe22c770935bce17b501ad8f6d63bd6f30c001878f0f3e5";
@@ -54,7 +53,6 @@ public class verifying {
 
     @Test
     void verifying_file() throws IOException {
-        System.out.println(3);
 
         File ver = new File("verify_test.txt");
         assert ver.createNewFile();
@@ -71,7 +69,6 @@ public class verifying {
 
     @Test
     void verifying_the_checksum() {
-        System.out.println(4);
 
         File ver = new File("verify_test.txt");
         hash Hash = new hash();
