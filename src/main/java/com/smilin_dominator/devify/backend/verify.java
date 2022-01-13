552537c15e8a8e0b9e4218940811488118277401
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2022-01-13 18:58:34 +0530

    feat: Added the License String to the source code /*     Devify     Copyright (C) 2021 Devisha Padmaperuma
        Devify is free software: you can redistribute it and/or modify
        it under the terms of the GNU General Public License as published by
        the Free Software Foundation, either version 3 of the License, or
        (at your option) any later version.
    
        Devify is distributed in the hope that it will be useful,
        but WITHOUT ANY WARRANTY; without even the implied warranty of
        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
        GNU General Public License for more details.
    
        You should have received a copy of the GNU General Public License
        along with Devify.  If not, see <https://www.gnu.org/licenses/>.
    */

--- src/main/java/com/smilin_dominator/devify/backend/verify.java
+++ src/main/java/com/smilin_dominator/devify/backend/verify.java
@@ -1,3 +1,20 @@
+/*
+    Devify
+    Copyright (C) 2021 Devisha Padmaperuma
+
+    Devify is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Devify is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Devify.  If not, see <https://www.gnu.org/licenses/>.
+*/
 package com.smilin_dominator.devify.backend;
 
 import javax.swing.*;
