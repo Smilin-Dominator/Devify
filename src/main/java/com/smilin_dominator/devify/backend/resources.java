a890387ba895a19dd829152ded833775324730f1
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2022-01-14 14:19:12 +0530

    feat: Completed the image function You have to pass in a subdir and the filename and it'll return the ImageIcon

--- src/main/java/com/smilin_dominator/devify/backend/resources.java
+++ src/main/java/com/smilin_dominator/devify/backend/resources.java
@@ -1,11 +1,13 @@
 package com.smilin_dominator.devify.backend;
 
 import javax.swing.*;
+import java.util.Objects;
 
 public class resources {
 
-    public ImageIcon image(String type, String ) {
-        Stream st = getClass().getResourceAsStream("/resources/")
+    public ImageIcon image(String subdir, String name) {
+        String path = "/%s/%s".formatted(subdir, name);
+        return new ImageIcon(Objects.requireNonNull(getClass().getResource(path)));
     }
 
 }
