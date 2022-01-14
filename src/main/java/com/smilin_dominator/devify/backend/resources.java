48ecb32e0e4e6df2b22bc1f1fc15d2fd2f54bb2f
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2022-01-14 13:57:15 +0530

    feat: Added a file that returns resources This will return any type of resource that's in the Resource Folder.

---
+++ src/main/java/com/smilin_dominator/devify/backend/resources.java
@@ -0,0 +1,11 @@
+package com.smilin_dominator.devify.backend;
+
+import javax.swing.*;
+
+public class resources {
+
+    public ImageIcon image(String type, String ) {
+        Stream st = getClass().getResourceAsStream("/resources/")
+    }
+
+}
