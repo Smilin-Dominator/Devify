5c8a8ba6b87dfa98456ddf4c2215ea62ddef03a6
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2022-01-14 14:25:14 +0530

    docs: Wrote documentation for the image function

--- src/main/java/com/smilin_dominator/devify/backend/resources.java
+++ src/main/java/com/smilin_dominator/devify/backend/resources.java
@@ -1,10 +1,18 @@
 package com.smilin_dominator.devify.backend;
 
-import javax.swing.*;
+import javax.swing.ImageIcon;
 import java.util.Objects;
 
 public class resources {
 
+    /**
+     * This returns an Image as an ImageIcon element, and crashes if no such image is found. For example,
+     * if you wanted to get 'resources/logos/foo.png' you have to pass 'logos' as the subdir and 'foo.png' as
+     * the name and it'll return the file as a 'javax.swing.ImageIcon'
+     * @param subdir The Subdirectory of the resources folder
+     * @param name The filename
+     * @return An ImageIcon with the specified file
+     */
     public ImageIcon image(String subdir, String name) {
         String path = "/%s/%s".formatted(subdir, name);
         return new ImageIcon(Objects.requireNonNull(getClass().getResource(path)));
