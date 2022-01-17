f64293c8d13a96fd6f4b7fe464934313be3ff4d4
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2022-01-17 22:50:45 +0530

    feat: Added the verify and hash buttons

--- src/main/java/com/smilin_dominator/devify/frontend/Main.java
+++ src/main/java/com/smilin_dominator/devify/frontend/Main.java
@@ -1,11 +1,15 @@
 package com.smilin_dominator.devify.frontend;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /** This is the main UI class. It returns the logo and a choice of hashing or verifying */
 public class Main extends JFrame implements ActionListener {
+
+     private hash HashClass = new hash();
+     private verify VerifyClass = new verify();
 
      public Main() {
 
@@ -14,21 +18,41 @@ public class Main extends JFrame implements ActionListener {
           setTitle("Devify - The Verifier!");
           setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-     }
+          // Main Container
+          JPanel overall = new JPanel();
+          overall.setLayout(new BoxLayout(overall, BoxLayout.Y_AXIS));
 
-     /**
-      * Sets the JFrame Visible
-      */
-     public void run() {
+          // The Buttons
+          JPanel buttonContainer = new JPanel();
+          buttonContainer.setLayout(new GridLayout(1, 2));
+
+          JButton verify = new JButton("Verify");
+          verify.addActionListener(this);
+
+          JButton hash = new JButton("Hash");
+          hash.addActionListener(this);
+
+          buttonContainer.add(verify);
+          buttonContainer.add(hash);
+
+          // Final Options
+          overall.add(buttonContainer);
+          this.add(overall);
           setVisible(true);
+
      }
 
      public void actionPerformed(ActionEvent e) {
-
+          String com = e.getActionCommand();
+          switch (com) {
+               case "Verify" -> VerifyClass.run();
+               case "Hash" -> HashClass.run();
+          }
+          this.dispose();
      }
 
      public static void main(String[] args) {
-
+          new Main();
      }
 
 }
