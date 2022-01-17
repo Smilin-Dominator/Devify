44547a849517c07490c27c24d2a0e4bf986215bf
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2022-01-17 21:50:15 +0530

    feat: Added a 'main' UI This will be the selection screen that allows you to select either verify or hash.

---
+++ src/main/java/com/smilin_dominator/devify/frontend/main.java
@@ -0,0 +1,30 @@
+package com.smilin_dominator.devify.frontend;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/** This is the main UI class. It returns the logo and a choice of hashing or verifying */
+public class main extends JFrame implements ActionListener {
+
+     public main() {
+
+          // Main Options
+          setSize(485,800);
+          setTitle("Devify - The Verifier!");
+          setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+     }
+
+     /**
+      * Sets the JFrame Visible
+      */
+     public void run() {
+          setVisible(true);
+     }
+
+     public void actionPerformed(ActionEvent e) {
+
+     }
+
+}
